b88d45204f7329e9cc79b6ddbc152cfec02c8f56
NHD <166632888+Namxnz@users.noreply.github.com>
2025-02-08 17:27:26 +0700

    Create census.R
    Feb/08/2025

---
+++ Data/census.R
@@ -0,0 +1,6 @@
+# Read the CSV file from the URL
+census <- read.csv("https://raw.githubusercontent.com/rashida048/Datasets/refs/heads/master/census.csv")
+# Check the first few rows
+head(census)
+# Data summary
+summary(census)
