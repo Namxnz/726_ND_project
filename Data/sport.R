79837eed2d0bfd699c94ac94e84fa80eaa852897
NHD <166632888+Namxnz@users.noreply.github.com>
2025-02-08 17:43:28 +0700

    Create sport.R
    Feb/8/2025

---
+++ Data/sport.R
@@ -0,0 +1,6 @@
+# Read the CSV file from the URL
+sports <- read.csv("https://people.sc.fsu.edu/~jburkardt/data/csv/mlb_players.csv")
+# Check the first few rows
+head(sports)
+# Data summary
+summary(sports)
